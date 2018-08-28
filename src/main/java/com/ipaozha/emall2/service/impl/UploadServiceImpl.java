bc9c4c2547723d3cf5173a35481b346d7b316703
suyikun <270081841@qq.com>
2018-08-28 18:03:41 +0800

    上传图片成功

--- src/main/java/com/ipaozha/emall2/service/impl/UploadServiceImpl.java
+++ src/main/java/com/ipaozha/emall2/service/impl/UploadServiceImpl.java
@@ -11,12 +11,12 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 
-@Service("uploadService")
+@Service
 public class UploadServiceImpl implements UploadService {
 
     @Value("${adImage.savePath}")
     private String savePath;
-    @Value("${adImage.urlsavePath}")
+    @Value("${adImage.url}")
     private String imageURL;
 
     @Autowired
