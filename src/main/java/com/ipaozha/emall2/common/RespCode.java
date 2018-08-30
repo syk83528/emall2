f7584066852bd9444b829963856fe0daacef4e50
suyikun <270081841@qq.com>
2018-08-30 18:02:01 +0800

    小提交

--- src/main/java/com/ipaozha/emall2/common/RespCode.java
+++ src/main/java/com/ipaozha/emall2/common/RespCode.java
@@ -1,4 +1,25 @@
 package com.ipaozha.emall2.common;
 
 public enum RespCode {
+    success(0,"success"),
+    error(1,"error"),
+    need_login(10,"need_login"),
+    illegal_arguments(2,"illegal_arguments");
+
+
+    private final Integer code;
+    private final String message;
+
+    RespCode(Integer code,String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
